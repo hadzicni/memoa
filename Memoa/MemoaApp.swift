313d18fd1c5d2a2b16a24a181130a8d35daea373
Nikola Hadzic <nikola.hadzic@proton.me>
2025-12-26 21:36:05 +0100

    Initial Commit

---
+++ Memoa/MemoaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoaApp.swift
+//  Memoa
+//
+//  Created by Nikola Hadzic on 26.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemoaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
